0cc654f7346fcf12012712532150ff7195442c74
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-05 20:14:27 +0300

    Initial Commit

---
+++ GitHubUsers/GitHubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersApp.swift
+//  GitHubUsers
+//
+//  Created by Ramill Ibragimov on 05.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
